cedd66c26be5a7ad5ec88766c1ee33e08a4dd04a
Yaredyy <Yaredyyehualashet@gmail.com>
2022-04-23 20:20:22 -0400

    update

---
+++ My2dGame/main/Main.java
@@ -0,0 +1,11 @@
+package My2dGame.main;
+
+import javax.swing.JFrame;
+
+public class Main{
+    public static void main(String[] args) {
+        
+        JFrame window = new JFrame();
+        window.
+    }
+}
