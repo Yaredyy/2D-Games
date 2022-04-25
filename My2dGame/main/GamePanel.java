5c0c8388feea485e0c94f4995b00eea8afa3f0ee
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2022-04-25 02:43:41 -0400

    tile, tileManagers, a bunch of images and files, and a new field map made :+1 :+1:

--- My2dGame/main/GamePanel.java
+++ My2dGame/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import My2dGame.entity.Player;
+import My2dGame.tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
     
@@ -16,23 +17,20 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale = 3;
     
     public final int tileSize = originalTileSize * scale; // 48*48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
 
-    final int screenWidth = tileSize*maxScreenCol; //768 pixels
-    final int screenHeight = tileSize*maxScreenRow; //576 pixels
+    public final int screenWidth = tileSize*maxScreenCol; //768 pixels
+    public final int screenHeight = tileSize*maxScreenRow; //576 pixels
 
     //FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
+
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this, keyH);
-
-    //set players default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
     
     //onstructor of gamePanel
     public GamePanel(){
@@ -88,6 +86,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D) g;
 
+        tileM.draw(g2);
         player.draw(g2);
 
         g2.dispose();
---
+++ My2dGame/tile/Tile.java
@@ -0,0 +1,10 @@
+package My2dGame.tile;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+
+    public BufferedImage image;
+    public boolean collision=false;
+    
+}
